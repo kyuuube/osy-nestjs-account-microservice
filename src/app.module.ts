60020aa56229e76679fd214ee6128ce29e444432
kyuuube <benchiu@yandex.com>
2020-01-31 22:55:13 +0800

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthUser } from './app.entity'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: 'account',
+      entities: [AuthUser],
+      host: process.env.DB_HOST,
+      password: process.env.DB_ADMIN_PASSWORD,
+      port: 3306,
+      synchronize: true,
+      type: 'mysql',
+      username: process.env.DB_ADMIN_USERNAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
