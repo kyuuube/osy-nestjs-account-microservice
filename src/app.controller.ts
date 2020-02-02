4a9b6c4b50d64531a693ced65b73899dfecc41fc
kyuuube <benchiu@yandex.com>
2020-02-03 04:48:45 +0800

    microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Logger } from '@nestjs/common'
 import { AppService } from './app.service'
+import { MessagePattern } from '@nestjs/microservices'
 
 @Controller()
 export class AppController {
+  private logger = new Logger('Account service')
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @MessagePattern({ cmd: 'login' })
+  public login(data: any) {
+    this.logger.log('log in' + data)
     return this.appService.getHello()
+  }
+
+  @MessagePattern({ cmd: 'sum' })
+  // Define the logic to be executed
+  async accumulate(data: number[])  {
+    this.logger.log('Adding ' + data.toString()); // Log something on every call
+    return this.appService.accumulate(data); // use math service to calc result & return
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  public getHello(): string {
     return 'Hello World!'
   }
+
+  public accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => Number(a) + Number(b));
+}
 }
